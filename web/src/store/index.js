065cb469872036cb35a0003ad42d730befd09664
Brandon Davis <brandon.davis@kitware.com>
2019-03-15 14:35:01 -0400

    Adds ability to set primary key

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -57,7 +57,9 @@ const mutations = {
 
     // Server doesn't guarantee order of indices.
     const rows = data.rows.sort((a, b) => a.row_index - b.row_index);
+    const row_key_index = rows.find(r => r.row_type === rowPrimaryKey);
     const cols = data.columns.sort((a, b) => a.column_index - b.column_index);
+    const col_key_index = cols.find(c => c.column_type === colPrimaryKey);
 
     // TODO: look for mutually exclusive transformations
     // pending #46
@@ -73,11 +75,11 @@ const mutations = {
       // user- and server-generated lables for rows and columns
       row: {
         labels: rows.map(r => r.row_type),
-        primary_key: null,
+        primary_key: row_key_index ? row_key_index.row_index : null,
       },
       column: {
         labels: cols.map(c => c.column_type),
-        primary_key: null,
+        primary_key: col_key_index ? col_key_index.column_index : null,
       },
       // JSON serialized copy of data.table
       sourcerows,
