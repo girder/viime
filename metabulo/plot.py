c78b58fe840ec5228ca199cb41b73c97e606c280
Jonathan Beezley <jonathan.beezley@kitware.com>
2019-03-22 11:20:35 -0400

    Use new API for generating PCA data

--- metabulo/plot.py
+++ metabulo/plot.py
@@ -1,15 +1,14 @@
+from numpy import sqrt
 from sklearn.decomposition import PCA
 
 
-def pca(measurements, metadata, x_component_index, y_component_index):
-    n_components = max(x_component_index, y_component_index) + 1
-    components = PCA(n_components=n_components).fit_transform(measurements)
-    samples = []
-    for i in range(measurements.shape[0]):
-        samples.append({
-            'x': components[i][x_component_index],
-            'y': components[i][y_component_index],
-            'key': measurements.index[i],
-            'labels': metadata.iloc[i].to_dict()
-        })
-    return samples
+def pca(measurements, max_components):
+    pca = PCA(n_components=max_components)
+    x = pca.fit_transform(measurements)
+    rotation = pca.components_.T
+    sdev = sqrt(pca.explained_variance_)
+    return {
+        'x': x.tolist(),
+        'rotation': rotation.tolist(),
+        'sdev': sdev.tolist()
+    }
