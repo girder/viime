c11cb1b462fe49ff7368916d062938a4005437bf
Jonathan Beezley <jonathan.beezley@kitware.com>
2019-04-02 09:53:55 -0400

    Add a hashing function for csv_file models

--- metabulo/cache.py
+++ metabulo/cache.py
@@ -7,10 +7,30 @@ from pandas.core.base import PandasObject
 from pandas.util import hash_pandas_object
 
 
+def hash_csv_file(csv_file):
+    """Return a unique checksum for a csv_file model.
+
+    This is designed to be as fast as possible, so it assumes the data
+    contained in the csv is immutable.  The cache needs to be invalidated
+    if the data can change.
+    """
+    # This will need to change if additional pipeline steps are added
+    components = [csv_file.uri, csv_file.normalization or '']
+    components += [r.row_type for r in csv_file.rows]
+    components += [c.column_type for c in csv_file.columns]
+
+    return sha256(''.join(components).encode()).hexdigest()
+
+
 def hash_argument(arg):
+    from metabulo.models import CSVFile  # circular import
+
     if isinstance(arg, PandasObject):
         r = hash_pandas_object(arg, index=True)
-        return sha256(r.values).hexdigest()
+        return sha256(r.values.tostring()).hexdigest()
+    elif isinstance(arg, CSVFile):
+        return hash_csv_file(arg)
+
     return str(arg)
 
 
