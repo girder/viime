4e109fe36ab39998ab21638dd8b6fff47f37f609
Brandon Davis <brandon.davis@kitware.com>
2019-04-11 14:44:40 -0400

    Add integration with sentry

--- web/src/main.js
+++ web/src/main.js
@@ -2,6 +2,8 @@ import '@mdi/font/css/materialdesignicons.min.css';
 
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import * as Sentry from '@sentry/browser';
+import { Vue as SentryVue } from '@sentry/integrations';
 
 import ApiService from './common/api.service';
 import App from './App.vue';
@@ -14,6 +16,11 @@ Vue.use(Vuetify, vuetifyConfig);
 Vue.config.productionTip = false;
 
 ApiService.init();
+
+Sentry.init({
+  dsn: 'https://9c7281f0dfc5402a953698256bd067e0@sentry.io/1437129',
+  integrations: [new SentryVue({ Vue })],
+});
 
 new Vue({
   router,
