d4377355c378ae54fa85cdc7cbe165a45c8182e1
Jeffrey Baumes <jeff.baumes@kitware.com>
2019-08-14 16:16:53 -0400

    Adding visualization tile component

---
+++ web/src/components/vis/VisTile.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<template lang="pug">
+v-flex(shrink=1)
+  .white.rounded
+    v-toolbar.primary.darken-3.top-rounded(dark, flat, dense)
+      v-toolbar-title {{ title }}
+    v-card(flat).rounded
+      slot
+</template>
+
+<style scoped lang="scss">
+.rounded {
+  border-radius: 5px;
+}
+
+.top-rounded {
+  border-radius: 5px 5px 0 0;
+}
+
+.vis-tile {
+  width: 600px;
+  max-width: 600px;
+  height: 648px;
+  max-height: 648px;
+}
+</style>
