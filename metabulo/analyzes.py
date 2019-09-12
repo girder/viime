204f87a2857e62e83af10f6170238c96958f00ad
Samuel Gratzl <sam@sgratzl.com>
2019-09-12 11:58:24 -0400

    first wilcoxon test version

--- metabulo/analyzes.py
+++ metabulo/analyzes.py
@@ -1,6 +1,7 @@
 from scipy.stats import wilcoxon
 import pandas as pd
 from typing import Dict, Any, Optional
+from itertools import combinations
 
 
 def wilcoxon_test(measurements: pd.DataFrame, zero_method: Optional[str] = None, alternative: Optional[str] = None) -> Dict[str, Any]:
@@ -8,7 +9,18 @@ def wilcoxon_test(measurements: pd.DataFrame, zero_method: Optional[str] = None,
         zero_method = 'wilcox'
     if alternative is None:
         alternative = 'two-sided'
-    # TODO 
+    
+    indices = list(measurements.index)
+    data = []
+
+    # test all possible combinations of rows
+    for (x, y) in combinations(measurements.iterrows(), 2):
+        x_index, x_data = x
+        y_index, y_data = y
+        (score, p) = wilcoxon(x_data, y_data, zero_method=zero_method, alternative=alternative)
+        data.append(dict(x=x_index,y=y_index,score=score,p=p))
+
     return {
-        'va': 5,
+        'indices': indices,
+        'data': data
     }
