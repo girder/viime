bd10f7c34d372fb72df72ede72c2b3953c792c61
Samuel Gratzl <sam@sgratzl.com>
2019-09-20 12:45:06 -0400

    fix warnings

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -54,8 +54,14 @@ export const routes = [
       },
       {
         path: 'analyze',
-        name: 'Analyze Data',
         component: RouterWrapper,
+        meta: {
+          breadcrumb() {
+            return {
+              text: 'Analyze Data',
+            };
+          },
+        },
         children: [
           {
             path: '',
