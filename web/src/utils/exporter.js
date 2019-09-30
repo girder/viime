a8088da49ac0b0483857701f3524ec41661e8098
Samuel Gratzl <sam@sgratzl.com>
2019-09-30 14:21:00 -0400

    export svg to pngs

---
+++ web/src/utils/exporter.js
@@ -0,0 +1,74 @@
+
+export function svg2png(svgElement, options = {}) {
+  const findStyles = options.styles !== false;
+  const returnSvg = options.return === 'svg';
+
+  // based on http://bl.ocks.org/biovisualize/8187844
+  const copy = svgElement.cloneNode(true);
+  // proper bg
+  copy.style.backgroundColor = 'white';
+  // inject font
+  copy.style.fontFamily = 'Barlow Condensed, sans-serif';
+  copy.insertAdjacentHTML('afterbegin', `<style>
+  @import url("https://fonts.googleapis.com/css?family=Barlow+Condensed");
+  </style>`);
+
+  // find related style sheets
+  const scopedAttr = Array.from(copy.getAttributeNames()).find(d => d.startsWith('data-v-'));
+  if (findStyles && scopedAttr) {
+    const key = `[${scopedAttr}]`;
+    const rules = [];
+    Array.from(document.styleSheets).forEach((sheet) => {
+      if (sheet instanceof CSSStyleSheet) {
+        try {
+          Array.from(sheet.cssRules).forEach((rule) => {
+            if (rule.cssText.includes(key)) {
+              rules.push(rule.cssText);
+            }
+          });
+        } catch {
+          // ignore
+        }
+      }
+    });
+    copy.insertAdjacentHTML('afterbegin', `<style>${rules.join('\n')}</style>`);
+  }
+
+  const svgString = new XMLSerializer().serializeToString(copy);
+  const svg = new Blob([svgString], { type: 'image/svg+xml;charset=utf-8' });
+  const svgUrl = URL.createObjectURL(svg);
+
+  if (returnSvg) {
+    return Promise.resolve(svgUrl);
+  }
+
+  const canvas = document.createElement('canvas');
+
+  const bb = svgElement.getBoundingClientRect();
+  canvas.width = bb.width;
+  canvas.height = bb.height;
+  const ctx = canvas.getContext('2d');
+  const img = new Image(canvas.width, canvas.height);
+
+  return new Promise((resolve) => {
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0);
+      URL.revokeObjectURL(svgUrl);
+      const png = canvas.toDataURL('image/png');
+      resolve(png);
+    };
+    img.src = svgUrl;
+  });
+}
+
+export async function downloadSVG(svgElement, title = 'Image') {
+  const url = await svg2png(svgElement);
+  const a = document.createElement('a');
+  a.href = url;
+  a.style.position = 'absolute';
+  a.style.left = '-10000px';
+  a.style.top = '-10000px';
+  a.download = `${title}.png`;
+  document.body.appendChild(a);
+  a.click();
+}
