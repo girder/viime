66d5fdb3f0a79fa0e308830d24b4589bcddd9c0c
Samuel Gratzl <sam@sgratzl.com>
2019-10-07 12:03:02 -0400

    ensure strings

--- viime/models.py
+++ viime/models.py
@@ -239,7 +239,7 @@ class CSVFile(db.Model):
         groups = self.groups
         if groups is None or groups.empty:
             return []
-        levels = sorted(groups.iloc[:, 0].unique())
+        levels = sorted([str(v) for v in groups.iloc[:, 0].unique()])
         # d3 scheme category 10
         colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2',
                   '#7f7f7f', '#bcbd22', '#17becf']
