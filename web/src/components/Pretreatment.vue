c8011bbd25c0bbf99b908bee4b0a64c3d7c378bb
Samuel Gratzl <sam@sgratzl.com>
2019-10-16 15:14:41 -0400

    wip

--- web/src/components/Pretreatment.vue
+++ web/src/components/Pretreatment.vue
@@ -18,10 +18,15 @@ export default {
     };
   },
   methods: {
+    stopPropagation(evt) {
+      evt.stopPropagation();
+    },
     valid(dataset) {
       return this.$store.getters.valid(dataset.id);
     },
-    navigate(dataset) {
+    navigate(evt, dataset) {
+      evt.preventDefault();
+      evt.stopPropagation();
       const { id } = dataset;
       this.$router.push({
         name: 'Pretreat Data',
@@ -52,112 +57,104 @@ export default {
 v-layout.pretreatment-component(row, fill-height)
 
   v-navigation-drawer.navigation(floating, permanent, style="min-width: 220px; width: 220px;")
-    v-layout(column, fill-height)
-      v-list.pt-0
-        v-list-group(v-for="(dataset, index) in datasets",
-            :key="dataset.id",
-            :value="dataset.id === id",
-            :append-icon="null",
-            @click="navigate(dataset)")
-          template(v-slot:activator)
-            v-list-tile.file-name
-              v-list-tile-title.title {{ dataset.name }}
-              v-list-tile-action(v-if="dataset.validation.length")
-                v-icon.pr-1(color="warning") {{ $vuetify.icons.warning }}
-              v-list-tile-action(v-else)
-                v-icon.pr-1(color="success") {{ $vuetify.icons.check }}
-          v-list.view-list
+    v-list(dense)
+      v-list-group(v-for="(dataset, index) in datasets",
+          :key="dataset.id",
+          :value="dataset.id === id")
+        template(#activator)
+          v-list-tile(:to="{ name: 'Pretreat Data', params: { id: dataset.id } }",
+              @click="stopPropagation")
+              v-list-tile-title
+                v-icon(color="warning", v-if="dataset.validation.length") {{ $vuetify.icons.warning }}
+                v-icon(color="success", v-else) {{ $vuetify.icons.check }}
+                | {{ dataset.name }}
 
-            v-list-tile.ml-2(
-                :class="{ active: $router.currentRoute.name === 'Clean Up Table' }",
-                @click="$router.push({ path: `/pretreatment/${dataset.id}/cleanup` })")
-              v-list-tile-title.pl-2
-                v-icon.pr-1.middle {{ $vuetify.icons.tableEdit }}
-                | Clean Up Table
+        v-list-tile(:to="{ name: 'Clean Up Table' }")
+          v-list-tile-title
+            v-icon.drawericon {{ $vuetify.icons.tableEdit }}
+            | Clean Up Table
 
-            v-list-tile.ml-4.my-1.px-0.small-tile(v-for="problemData in dataset.validation",
-                @click="problemNav(problemData)",
-                :class="{ active: problemData.type === problem }",
-                :inactive="!problemData.clickable",
-                :key="problemData.title")
-              v-list-tile-title.small-tile.px-0
-                v-icon.pr-1.middle(
-                    :color="problemData.severity") {{ $vuetify.icons[problemData.severity] }}
-                | {{ problemData.title }}
-                span.pl-1(v-if="problemData.data") ({{ problemData.data.length }})
-                v-tooltip(v-else, top)
-                  template(v-slot:activator="{ on }")
-                    v-icon.pl-1(small, @click="", v-on="on") {{ $vuetify.icons.info }}
-                  span {{ problemData.description }}
+        v-list-tile.small-tile(v-for="problemData in dataset.validation",
+            @click="problemNav(problemData)",
+            :class="{ active: problemData.type === problem }",
+            :inactive="!problemData.clickable",
+            :key="problemData.title")
+          v-list-tile-title
+            v-icon.drawericon(
+                :color="problemData.severity") {{ $vuetify.icons[problemData.severity] }}
+            | {{ problemData.title }}
+            span(v-if="problemData.data") ({{ problemData.data.length }})
+            v-tooltip(v-else, top)
+              template(#activator="{ on }")
+                v-icon(small, @click="", v-on="on") {{ $vuetify.icons.info }}
+              span {{ problemData.description }}
 
-            v-list-tile.ml-2(
-                :class="{ active: $router.currentRoute.name === 'Transform Table' }",
-                :disabled="!valid(dataset)",
-                @click="$router.push({ path: `/pretreatment/${dataset.id}/transform` })")
-              v-list-tile-title.pl-2
-                v-icon.pr-1.middle {{ $vuetify.icons.bubbles }}
-                | Transform Table
+        v-list-tile(:to="{ name: 'Transform Table' }",
+            :disabled="!valid(dataset)")
+          v-list-tile-title
+            v-icon.pr-1.drawericon {{ $vuetify.icons.bubbles }}
+            | Transform Table
 
-            v-list-group.ml-2.link-group(
-                :append-icon="null",
-                :value="isSubRoute(dataset, `analyze/`)",
-                :disabled="!valid(dataset)",
-                @click="$router.push({ path: `/pretreatment/${dataset.id}/analyze` })")
-              template(v-slot:activator)
-                v-list-tile(
-                   :class="{ active: $router.currentRoute.name === 'Analyze Data' }")
-                  v-list-tile-title.pl-2
-                    v-icon.pr-1.middle {{ $vuetify.icons.cogs }}
-                    | Analyze Table
-              v-list-tile.ml-2.small-tile(
-                  v-for="a in analyses", :key="a.path",
-                  :class="{ active: $router.currentRoute.name === a.shortName }",
-                  @click="$router.push({ path: `/pretreatment/${dataset.id}/analyze/${a.path}` })")
-                v-list-tile-title.pl-2
-                  v-icon.pr-1.middle {{ $vuetify.icons.compare }}
-                  | {{a.shortName}}
+        v-list-group(sub-group,
+            :disabled="!valid(dataset)",
+            group=".*/analyze/.*"
+            :value="true")
+          template(#activator)
+            v-list-tile(:to="{ name: 'Analyze Data' }",
+              @click="stopPropagation")
+              v-list-tile-title
+                v-icon.drawericon {{ $vuetify.icons.cogs }}
+                | Analyze Table
+          v-list-tile.small-tile(v-for="a in analyses", :key="a.path",
+              :to="{ name: a.shortName }",
+              :disabled="!valid(dataset)")
+            v-list-tile-title
+              v-icon.drawericon {{ $vuetify.icons.compare }}
+              | {{a.shortName}}
 
   keep-alive
     router-view
 </template>
 
 <style lang="scss">
-.pretreatment-component .navigation {
-  i.middle {
+.navigation {
+  .drawericon {
     vertical-align: top;
-  }
-  .small-tile .v-list__tile {
-    height: 32px;
+    margin-right: 2px;
   }
 
-  .active {
-    background-color: #37474f;
-    border-radius: 40px 0 0 40px;
-    color: white;
-    i {
-      color: white;
-    }
-  }
-  .file-name .v-list__tile {
-    padding-right: 0;
+  // .small-tile .v-list__tile {
+  //   height: 32px;
+  // }
 
-    .v-list__tile__action{
-      min-width: 0;
-      padding-right: 16px;
-    }
-  }
+  // .active {
+  //   background-color: #37474f;
+  //   border-radius: 40px 0 0 40px;
+  //   color: white;
+  //   i {
+  //     color: white;
+  //   }
+  // }
+  // .file-name .v-list__tile {
+  //   padding-right: 0;
 
-  .view-list .v-list__tile--link,
-  .view-list .link-group > .v-list__group__header {
-    transition: none;
+  //   .v-list__tile__action {
+  //     min-width: 0;
+  //     padding-right: 16px;
+  //   }
+  // }
 
-    .v-list__tile__title {
-      transition: none;
-    }
+  // .view-list .v-list__tile--link,
+  // .view-list .link-group > .v-list__group__header {
+  //   transition: none;
 
-    &:hover {
-      border-radius: 40px 0 0 40px;
-    }
-  }
+  //   .v-list__tile__title {
+  //     transition: none;
+  //   }
+
+  //   &:hover {
+  //     border-radius: 40px 0 0 40px;
+  //   }
+  // }
 }
 </style>
