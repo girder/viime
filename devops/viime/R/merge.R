efd014fcdd000e050e34a8ce5e0ac9874780eb57
Samuel Gratzl <sam@sgratzl.com>
2019-10-17 14:47:43 -0400

    drop useless PCs

--- devops/viime/R/merge.R
+++ devops/viime/R/merge.R
@@ -9,7 +9,8 @@ compute_clean_pca <- function(prefix, MS_metab) {
   #PCA for MS
   ###
   #Perform pca
-  MS.pca <- prcomp(MS_metab, center = TRUE) #,scale. = TRUE #Center takes the mean of the column to all the values in the column
+  # also omit constant PCs
+  MS.pca <- prcomp(MS_metab, center = TRUE, tol = 0) #,scale. = TRUE #Center takes the mean of the column to all the values in the column
   
   #Saving the scores
   MS.pca.scores <- MS.pca$x
@@ -18,8 +19,11 @@ compute_clean_pca <- function(prefix, MS_metab) {
   #getting the standard deviations of the principal components (the square roots of the eigenvalues)
   MS.pca.sd <- MS.pca$sdev
   
+  # also omit constant PCs
+  MS.pca.sd = MS.pca.sd[MS.pca.sd > 0]
+  
   #getting the eigenvalues
-  MS.eig <- (MS.pca$sdev)^2
+  MS.eig <- (MS.pca.sd)^2
   
   #transform the eigenvalues in Variances in percentage (0-1)
   MS.variance <- MS.eig/sum(MS.eig)
