f491fa63962e7c8197eff9c86d0d25a3bc0b7ced
Samuel Gratzl <sam@sgratzl.com>
2019-10-24 08:42:40 -0400

    provide R service in circleci

---
+++ tests/test_opencpu.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas
+from pandas.testing import assert_frame_equal
+
+from viime.opencpu import opencpu_request
+
+
+def test_echo(app):
+    df = pandas.DataFrame(np.random.randint(0, 100, size=(10, 4)), columns=list('ABCD'))
+
+    with app.app_context():
+        out = opencpu_request('echo', {
+            'table': df.to_csv().encode()
+        })
+        assert_frame_equal(df, out)
+        raise AssertionError('dummz')
