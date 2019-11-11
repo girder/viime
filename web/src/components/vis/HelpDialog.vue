904da86a6d6835a59790502862876f32fabe6edb
Samuel Gratzl <sam@sgratzl.com>
2019-11-11 11:58:31 -0500

    extract help dialog

---
+++ web/src/components/vis/HelpDialog.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      showHelp: false,
+    };
+  },
+};
+</script>
+
+<template lang="pug">
+v-dialog(v-model="showHelp", max-width="33vw")
+  template(v-slot:activator="{ on }")
+    v-btn(v-on="on", icon)
+      v-icon {{ $vuetify.icons.help }}
+  v-card
+    v-card-title
+      h3.headline {{ title }}
+    v-card-text
+      slot
+    v-card-actions
+      v-spacer
+      v-btn(@click="showHelp = false") Close
+</template>
