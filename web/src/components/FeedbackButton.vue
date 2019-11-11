5e61ce24ae03f134e2eeb1e72f1237a63e627c01
Samuel Gratzl <sam@sgratzl.com>
2019-11-11 10:52:55 -0500

    tune landing page

--- web/src/components/FeedbackButton.vue
+++ web/src/components/FeedbackButton.vue
@@ -9,21 +9,28 @@ export default {
   data() {
     return {
       visible: false,
-      src: 'https://docs.google.com/forms/d/e/1FAIpQLScWccZqTF_b6nQkfkAvLNf7XGV9k7Rqf8V_Mid-oZLY47g_IQ/viewform?embedded=true',
+      form: 'https://docs.google.com/forms/d/e/1FAIpQLScWccZqTF_b6nQkfkAvLNf7XGV9k7Rqf8V_Mid-oZLY47g_IQ/viewform',
       width: 640,
       height: 930,
     };
+  },
+  computed: {
+    src() {
+      return `${this.form}?embedded=true`;
+    },
   },
 };
 </script>
 
 <template lang="pug">
-v-dialog(v-model="visible", :width="width", lazy)
+v-btn(v-if="front", :href="form", depressed, large,
+    target="_blank", rel="noopener noreferrer")
+  v-icon.mr-2(left) {{ $vuetify.icons.feedback }}
+  | Contact us
+
+v-dialog(v-else, v-model="visible", :width="width", lazy)
   template(#activator="{ on }")
-    v-btn.mt-3.mx-0.px-3(v-on="on", v-if="front", depressed, large)
-      v-icon.mr-2(left) {{ $vuetify.icons.feedback }}
-      | Contact us
-    v-btn(v-on="on", icon, title="submit feedback", v-else)
+    v-btn(v-on="on", icon, title="submit feedback")
       v-icon {{ $vuetify.icons.feedback }}
 
   iframe.iframe(:src="src", :width="width", :height="height",
--- web/src/components/Landing.vue
+++ web/src/components/Landing.vue
@@ -1,5 +1,10 @@
 <script>
+import FeedbackButton from './FeedbackButton.vue';
+
 export default {
+  components: {
+    FeedbackButton,
+  },
   data() {
     return {
       capabilityClasses: {
@@ -170,22 +175,20 @@ v-app.viime-landing
 
   .collaboration
     v-container
-      v-layout(row)
-        v-flex(pa-3, sm6)
+      v-layout(row, wrap)
+        v-flex(md8, xs12)
           h2.mb-3.pa-0 Ready for Collaboration
           p The code for VIIME is in a permissive open source library, meaning it can be used and
             | extended across academia and industry with no restrictions. This includes deploying
             | VIIME at your institution for your internal research team. If you'd like to partner
             | with us to deploy or extend VIIME
-          v-btn.mt-3.mx-0.px-3(href="https://github.com/girder/viime", depressed, large,
-              target="_blank", rel="noopener noreferrer")
-            v-icon.mr-2(left) $vuetify.icons.github
-            | View on GitHub
-        v-flex(pa-3, sm6)
-          v-text-field(label="text field", placeholder="Placeholder", solo, dense)
-          v-text-field(label="text field", placeholder="Placeholder", solo, dense)
-          v-text-field(label="text field", placeholder="Placeholder", solo, dense)
-          v-btn.mx-0(color="accent lighten-1", depressed, large) join us
+        v-flex.contacts(md4, xs12)
+          div
+            feedback-button(front)
+            v-btn(href="https://github.com/girder/viime", depressed, large,
+                target="_blank", rel="noopener noreferrer")
+              v-icon.mr-2(left) $vuetify.icons.github
+              | View on GitHub
 
   .footer
     p.ma-0.pa-3 &copy; 2019 Kitware, Inc. | &lt; License Info Here &gt;
@@ -346,6 +349,18 @@ v-app.viime-landing
     top: 0;
     transform: translateX(-50%);
     width: 0;
+  }
+}
+
+.contacts {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
   }
 }
 
