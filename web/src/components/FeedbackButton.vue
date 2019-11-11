906cb5229d1e0c36d51586b2b4cd6292f56b43fe
Samuel Gratzl <sam@sgratzl.com>
2019-11-11 10:37:22 -0500

    update form

--- web/src/components/FeedbackButton.vue
+++ web/src/components/FeedbackButton.vue
@@ -1,22 +1,33 @@
 <script>
 export default {
+  props: {
+    front: {
+      type: Boolean,
+      default: false,
+    },
+  },
   data() {
     return {
       visible: false,
       src: 'https://docs.google.com/forms/d/e/1FAIpQLScWccZqTF_b6nQkfkAvLNf7XGV9k7Rqf8V_Mid-oZLY47g_IQ/viewform?embedded=true',
+      width: 640,
+      height: 930,
     };
   },
 };
 </script>
 
 <template lang="pug">
-v-dialog(v-model="visible", width="640", lazy)
+v-dialog(v-model="visible", :width="width", lazy)
   template(#activator="{ on }")
-    v-btn(v-on="on", icon, title="submit feedback")
+    v-btn.mt-3.mx-0.px-3(v-on="on", v-if="front", depressed, large)
+      v-icon.mr-2(left) {{ $vuetify.icons.feedback }}
+      | Contact us
+    v-btn(v-on="on", icon, title="submit feedback", v-else)
       v-icon {{ $vuetify.icons.feedback }}
 
-  iframe.iframe(:src="src",
-      width="640", height="891", frameborder="0", marginheight="0", marginwidth="0")
+  iframe.iframe(:src="src", :width="width", :height="height",
+      frameborder="0", marginheight="0", marginwidth="0")
 </template>
 <style lang="scss" scoped>
 .iframe {
