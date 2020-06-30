6df3e0d7876b46691cc0298a139bc3b73d93b033
JellyWang-3 <jelly.wang@kitware.com>
2020-06-30 17:57:45 -0400

    could not click button
    Setting up 'clickXPath' for vBtn

---
+++ e2e-test/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: 'jest-puppeteer',
+  // jest-puppeteer manages the testEnvironment variable, so be sure not to specify it
+
+  setupFilesAfterEnv: [
+    // Normally, expect-puppeteer is specified by the jest-puppeteer preset
+    'expect-puppeteer',
+    './jest-puppeteer-vuetify',
+    './jest.setup.js',
+  ],
+
+  transformIgnorePatterns: [
+    // Apply Babel transformation to jest-puppeteer-vuetify
+    'node_modules/(?!jest-puppeteer-vuetify/)',
+  ],
+
+  // extend test timeout to 1 hour when debugging
+  testTimeout: (process.env.DEBUG === 'true') ? 3600000 : 180000,
+};
