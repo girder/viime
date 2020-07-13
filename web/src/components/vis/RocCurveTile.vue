be66e7e742560482e3bc8c770721e5669d0755ab
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2020-07-13 14:39:57 -0400

    add selector for method

--- web/src/components/vis/RocCurveTile.vue
+++ web/src/components/vis/RocCurveTile.vue
@@ -31,15 +31,28 @@ export default {
       location: location.href,
       groups: [],
       rocData: {},
+      method: 'random_forest',
+      methodOptions: [
+        { value: 'random_forest', text: 'Random Forest' },
+        { value: 'logistic_regression', text: 'Logistic Regression' },
+      ],
     };
   },
 
   watch: {
-    async search(newSearch) {
+    search(newSearch) {
       if (!newSearch || newSearch.length === 0) {
         this.rocData = null;
         return;
       }
+      this.updateRoc();
+    },
+    method() {
+      this.updateRoc();
+    }
+  },
+  methods: {
+    async updateRoc() {
       const id = this.location.split('/')[this.location.split('/').length - 3];
       const apiUrl = new URL('api/v1', process.env.VUE_APP_SERVER_ADDRESS).href;
 
@@ -47,16 +60,15 @@ export default {
         method: 'get',
         url: `${apiUrl}/csv/${id}/analyses/roc`,
         params: {
-          column: newSearch,
+          column: this.search,
           group: this.groups[0],
-          method: 'random_forest', // TODO
+          method: this.method,
         },
       };
       const unformattedData = (await axios(config)).data;
       this.rocData = unformattedData;
-    },
+    }
   },
-
   async mounted() {
     // quick and dirty way to grab file id
     const id = this.location.split('/')[this.location.split('/').length - 3];
@@ -74,17 +86,22 @@ vis-tile-large(title="ROC Curve", expanded)
     v-toolbar.darken-3(color="primary", dark, flat, dense, :card="false")
       v-toolbar-title Metabolite
     v-card.mx-3.px-2(flat)
-      div
-        v-autocomplete(
-            v-model="search",
-            :search-input.sync="currentUserInput",
-            :items="columns.filter(column => column.column_type === 'measurement').map(column => column.column_header)",
-            chips,
-            dense,
-            deletable-chips,
-            auto-select-first,
-            hide-selected,
-            hide-details,)
+      v-autocomplete(
+          v-model="search",
+          :items="columns.filter(column => column.column_type === 'measurement').map(column => column.column_header)",
+          chips,
+          dense,
+          deletable-chips,
+          auto-select-first,
+          hide-selected,
+          hide-details,)
+    v-toolbar.darken-3(color="primary", dark, flat, dense, :card="false")
+      v-toolbar-title Method
+    v-card.mx-3.px-2(flat)
+      v-select.py-2(
+        hide-details,
+        v-model="method",
+        :items="methodOptions")
   roc-curve(:roc-data="rocData")
 </template>
 
