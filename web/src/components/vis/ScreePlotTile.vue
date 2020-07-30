046596a9f51c019fea5d0130bb498568e0169db6
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-07-30 17:31:13 -0400

    Fix pug linting

--- web/src/components/vis/ScreePlotTile.vue
+++ web/src/components/vis/ScreePlotTile.vue
@@ -33,26 +33,14 @@ export default defineComponent({
 </script>
 
 <template lang="pug">
-vis-tile(
-  v-if="plot",
-  title="PCA Scree Plot",
-  :loading="plot.loading",
-  svg-download
-)
+vis-tile(v-if="plot", title="PCA Scree Plot", :loading="plot.loading", svg-download)
   scree-plot(
-    v-if="plot.data",
-    :eigenvalues="plot.data.sdev",
-    :num-components="numComponents",
-    :show-cutoffs="showCutoffs"
-  )
+      v-if="plot.data",
+      :eigenvalues="plot.data.sdev",
+      :num-components="numComponents",
+      :show-cutoffs="showCutoffs")
   template(v-slot:controls)
-    v-menu(
-      bottom,
-      offset-y,
-      left,
-      :min-width="150",
-      :close-on-content-click="false"
-    )
+    v-menu(bottom, offset-y, left, :min-width="150", :close-on-content-click="false")
       template(v-slot:activator="{ on }")
         v-btn(v-on="on", icon)
           v-icon.mdi.mdi-dots-vertical
@@ -60,18 +48,13 @@ vis-tile(
       v-card.pa-1(flat)
         v-layout.px-2(column)
           v-text-field.py-2(
-            hide-details,
-            type="number",
-            label="Principal Components",
-            min="1",
-            outline,
-            v-model="numComponentsText"
-          )
-          v-switch.py-2(
-            v-model="showCutoffs",
-            label="Diagnostic cutoffs",
-            hide-details
-          )
+              hide-details,
+              type="number",
+              label="Principal Components",
+              min="1",
+              outline,
+              v-model="numComponentsText")
+          v-switch.py-2(v-model="showCutoffs", label="Diagnostic cutoffs", hide-details)
   template(v-slot:help)
     include help/ScreePlotHelp.pug
 </template>
