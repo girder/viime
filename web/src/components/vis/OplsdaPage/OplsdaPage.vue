a156e4ce152b4ec4cc3cd7a5b7e54e62faf6db4d
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-10-05 11:34:12 -0400

    Do not assume initial OPLSDA groups on frontend

--- web/src/components/vis/OplsdaPage/OplsdaPage.vue
+++ web/src/components/vis/OplsdaPage/OplsdaPage.vue
@@ -34,8 +34,8 @@ export default defineComponent({
       showCutoffs: true,
       showScore: true,
       showLoadings: true,
-      group1: dataset.value?.groupLevels[0]?.name || '',
-      group2: dataset.value?.groupLevels[1]?.name || '',
+      group1: dataset.value?.groupLevels[0]?.name || null,
+      group2: dataset.value?.groupLevels[1]?.name || null,
     });
 
     const ready = computed(() => {
@@ -58,7 +58,7 @@ export default defineComponent({
     const eigenvalues = computed(() => plot.value.data?.scores.sdev || []);
     const rowLabels = computed(() => plot.value.data?.rows || []);
     const groupLabels = computed(() => plot.value.data?.labels || {});
-    const columns = computed(() => dataset.value?.column.data || []);
+    const columns = computed(() => dataset.value?.column?.data || []);
     const groupLevels = computed(() => dataset.value?.groupLevels || []);
     const groupNames = computed(() => groupLevels.value.map(
       (level: { name: string }) => level.name,
@@ -91,7 +91,8 @@ export default defineComponent({
         controls.group2 = oldGroup;
         changePlotArgs({ group2: oldGroup });
       }
-      if (plot.value) {
+      // only force a refresh if both groups are set
+      if (plot.value && controls.group1 && controls.group2) {
         plot.value.valid = false;
       }
     });
@@ -102,16 +103,10 @@ export default defineComponent({
         controls.group1 = oldGroup;
         changePlotArgs({ group1: oldGroup });
       }
-      if (plot.value) {
+      // only force a refresh if both groups are set
+      if (plot.value && controls.group1 && controls.group2) {
         plot.value.valid = false;
       }
-    });
-
-    // If no groups are selected after the plot loads, select the first two groups
-    watch(() => dataset.value, () => {
-      controls.group1 = dataset.value?.groupLevels[0]?.name || '';
-      controls.group2 = dataset.value?.groupLevels[1]?.name || '';
-      changePlotArgs({ group1: controls.group1, group2: controls.group2 });
     });
 
     return {
